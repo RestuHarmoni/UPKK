f676af5563559512abd14f4022febef61f9932b7
RestuHarmoni <restuharmoni@gmail.com>
2026-05-29 01:33:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'upkk-smartkids-admin-question-manager-v3';
+const CACHE_NAME = 'upkk-smartkids-admin-question-manager-v2-desktop-stable';
 const CORE_ASSETS = [
   './',
   './index.html',
